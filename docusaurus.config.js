3505433b31db336beaf898e620e28b76a05d8175
Abby <78209557+abby-cyber@users.noreply.github.com>
2022-11-14 14:17:06 +0800

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  url: 'https://abby-cyber.github.io/', // 你的网站 URL
+  baseUrl: '/',
+  projectName: 'abby-cyber.github.io',
+  organizationName: 'abby-cyber',
+  trailingSlash: false,
+  // ...
+};
