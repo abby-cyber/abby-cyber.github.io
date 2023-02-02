8ab5c0e6641dc8169f1d47b6f78e4890cb401da7
abby.huang <78209557+abby-cyber@users.noreply.github.com>
2023-02-02 17:15:51 +0800

    Delete docusaurus.config.js

--- docusaurus.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // ...
-  url: 'https://abby-cyber.github.io/', // 你的网站 URL
-  baseUrl: '/',
-  projectName: 'abby-cyber.github.io',
-  organizationName: 'abby-cyber',
-  trailingSlash: false,
-  // ...
-};
